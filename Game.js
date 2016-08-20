ef52f9c7744c77a275d6b39fc4a7131cbca75da2
AJ Beckner <ajbeckner@gmail.com>
2016-08-20 17:37:13 -0400

    added timer and score functionality

--- Game.js
+++ Game.js
@@ -35,34 +35,27 @@ SpriteAnim.Game.prototype = {
 
         ////////////////////////////
 
-        var result = this.findObjectsByType('playerStart', this.map, 'Objects');
-        //
-        this.hero = this.game.add.sprite(result[0].x, result[0].y, 'guywalking');
+        var playerPosition = this.findObjectsByType('playerStart', this.map, 'Objects');
+        var enemyPositions = this.findObjectsByType('enemy1', this.map, 'Objects');
 
-        // this.hero = this.add.sprite(50, 960, 'guywalking');
-        this.physics.arcade.enable(this.hero);
 
-        //Change the world size to match the size of this layer
-        this.groundLayer.resizeWorld();
-
-        //Set some physics on the sprite
-        this.hero.body.bounce.y = 0.2;
-        this.hero.body.gravity.y = 2000;
-
-        this.hero.animations.add('left', [0, 1, 2], 10, true);
-        this.hero.animations.add('right', [3, 4, 5], 10, true);
-        this.hero.scale.x = 2;
-        this.hero.scale.y = 2;
-        //Make the camera follow the sprite
+        this.enemies = this.makeEnemies(enemyPositions);
+        this.makeHero(playerPosition);
         this.camera.follow(this.hero);
-
-        this.camera.focusOnXY(50,1080);
 
         //Enable cursor keys so we can create some controls
         this.cursors = this.input.keyboard.createCursorKeys();
 
+        this.createScoreText();
+        this.createTimeText();
+
         this.winButton = this.add.button(10,this.world.height - 100,'myButton',this.displayWinText,this,1,0,2);
         this.loseButton = this.add.button(960 - 100,this.world.height - 100,'myButton',this.displayLoseText,this,4,3,5);
+        this.loseButton = this.add.button(this.camera.width/2 ,this.world.height - 100,'myButton',this.addScore,this,4,3,5);
+
+        this.score = 0;
+        this.totalSeconds = 0;
+        this.time.events.loop(Phaser.Timer.SECOND, this.incrementTime, this);
     },
 
     update: function () {
@@ -79,19 +72,23 @@ SpriteAnim.Game.prototype = {
 
         //Make the sprite collide with the ground layer
         this.physics.arcade.collide(this.hero, this.groundLayer);
-
+        for (var i = 0; i < this.enemies.length; i++) {
+            this.physics.arcade.collide(this.enemies[i], this.groundLayer);
+        }
         this.moveHero();
-        // this.checkWin();
+        this.checkWin();
+        this.updateScoreText();
+        this.updateTimeText();
     },
 
-    displayWinText: function (pts) {
-        this.displayGameEndText(' Congratulations! our her0\n has made it t0 the g0al!\n\n Y0u g0t: 540 points!\n\n Shall you try and best\n yourself, brave player?');
+    displayWinText: function () {
+        this.displayGameEndText(' Congratulations! our her0\n has made it t0 the g0al!\n\n Y0u g0t: ' + this.score +' points!\n\n Shall you try and best\n yourself, brave player?');
     },
 
-    displayLoseText: function (pts) {
-        // this.hero.visible = false;
+    displayLoseText: function () {
+        this.hero.visible = false;
         this.stage.backgroundColor = 0x993333;
-        this.displayGameEndText(' Oh n0, 0ur her0 has fallen!\n\n Y0u g0t: 70 points\n\n Have y0u the heart to g0 0n,\n brave player?');
+        this.displayGameEndText(' Oh n0, 0ur her0 has fallen!\n\n Y0u g0t: ' + this.score +' points\n\n Have y0u the heart to g0 0n,\n brave player?');
     },
 
     displayGameEndText: function (text) {
@@ -111,6 +108,54 @@ SpriteAnim.Game.prototype = {
         this.replayButton.smoothed = false;
         this.replayButton.tint = tint;
         this.input.onDown.addOnce(this.start, this);
+    },
+
+    createScoreText: function () {
+        this.scoreText = this.add.bitmapText(this.camera.x + 20, this.camera.y + 70, 'interfont', "Score: 0", 40);
+        this.scoreText.anchor.x = 0;
+        this.scoreText.anchor.y = 0;
+        this.scoreText.smoothed = false;
+        this.scoreText.tint = 0x222222;
+    },
+
+    updateScoreText: function () {
+        this.scoreText.x = this.camera.x + 20;
+        this.scoreText.y = this.camera.y + 70;
+        this.scoreText.setText("Score: " + this.score)
+    },
+
+    createTimeText: function () {
+        this.timeText = this.add.bitmapText(this.camera.x + 20, this.camera.y + 20, 'interfont', "Time: 02:00", 40);
+        this.timeText.anchor.x = 0;
+        this.timeText.anchor.y = 0;
+        this.timeText.smoothed = false;
+        this.timeText.tint = 0x222222;
+    },
+
+    updateTimeText: function () {
+        this.timeText.x = this.camera.x + 20;
+        this.timeText.y = this.camera.y + 20;
+        // this.totalSeconds = Math.floor(this.totalSeconds++);
+        // this.totalSeconds++;
+        var totalSecondsLeft = 120 - this.totalSeconds;
+        var minutes = Math.floor(totalSecondsLeft / 60);
+        var seconds = Math.floor(totalSecondsLeft % 60);
+        if (minutes < 10) { minutes = "0" + minutes}
+        if (seconds < 10) { seconds = "0" + seconds}
+        this.timeText.setText("Time: " + minutes + ":" +seconds);
+    },
+    incrementTime: function () {
+        if(!this.gameEndText){
+            if (this.totalSeconds < 120) {
+                this.totalSeconds++;
+            } else {
+                this.displayLoseText()
+            }
+        }
+    },
+
+    addScore: function () {
+        this.score += 50;
     },
 
     start: function () {
@@ -141,25 +186,55 @@ SpriteAnim.Game.prototype = {
     },
 
     checkWin: function () {
-        if(this.hero.body.x > 400) {
+        if(this.hero.body.x > 5400) {
             this.displayWinText(537);
-        }
-        if(this.hero.body.x < 40) {
-            this.displayLoseText(66);
         }
     },
 
     findObjectsByType: function(type, map, layer) {
         var result = new Array();
-        console.log(JSON.stringify(map.objects[layer]));
         map.objects[layer].forEach(function(element){
-            console.log(JSON.stringify(element));
             if(element.type === type) {
                 element.y -= map.tileHeight;
                 result.push(element);
             }
         });
         return result;
+    },
+
+    makeHero: function (result) {
+        this.hero = this.game.add.sprite(result[0].x, result[0].y, 'guywalking');
+
+        this.physics.arcade.enable(this.hero);
+        this.hero.body.bounce.y = 0.2;
+        this.hero.body.gravity.y = 2000;
+
+        this.hero.animations.add('left', [0, 1, 2], 10, true);
+        this.hero.animations.add('right', [3, 4, 5], 10, true);
+
+        this.hero.scale.x = 2;
+        this.hero.scale.y = 2;
+
+        this.camera.follow(this.hero);
+
+    },
+
+    makeEnemies: function (results) {
+        var enemies = [];
+        for (var i = 0; i < results.length; i++) {
+            var enemy = this.game.add.sprite(results[i].x, results[i].y, 'guywalking');
+            this.physics.arcade.enable(enemy);
+            enemy.body.bounce.y = 0.2;
+            enemy.body.gravity.y = 2000;
+
+            enemy.animations.add('left', [0, 1, 2], 10, true);
+            enemy.animations.add('right', [3, 4, 5], 10, true);
+
+            enemy.scale.x = 2;
+            enemy.scale.y = 2;
+            enemies.push(enemy);
+        }
+        return enemies;
     }
 
 };
